8fefc3955b9a81910fb4f60082edd5ca8b2b70f3
raissa620-d <deborahdzahini2@gmail.com>
2025-08-02 05:17:15 +0200

    ur

--- JobSearch/JobSearch/settings.py
+++ JobSearch/JobSearch/settings.py
@@ -79,16 +79,16 @@ WSGI_APPLICATION = 'JobSearch.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES = {
+'''DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
-}
+}'''
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'debi$Jobsearch',
+        'NAME': 'debi$debi',
         'USER': 'debi',
         'PASSWORD': 'urbain2@',
         'HOST': 'debi.mysql.pythonanywhere-services.com',
